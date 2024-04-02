69a123885b7a0caa8804aab7213f172b216d5497
danielcruz94 <daniel94cruz@gmail.com>
2024-04-02 17:37:28 -0500

    usuario no encontrado

--- Controllers/getProducts.js
+++ Controllers/getProducts.js
@@ -21,32 +21,37 @@ const User= require('../models/Users')
 
 const getPriceSpecial = async (request, response) => {
     try {
-        const { user_id, nombre_producto } = request.params;
-        if (!(user_id || nombre_producto)) {
-            return response.status(400).json({ message: "faltan datos" });
-        }
-
-        const userFilter = await User.findById(user_id);
-
-        const encodedNombreProducto = encodeURIComponent(nombre_producto);
-        const decodedNombreProducto = decodeURIComponent(nombre_producto.replace(/\+/g, ' '));
-
-        const listProducts = await Product.find({ name: decodedNombreProducto });
-
-        // Si la lista de productos está vacía, significa que no se encontró el producto
-        if (listProducts.length === 0) {
-            return response.status(404).json({ message: "Producto no encontrado" }); // Responde 404 si no hay productos
-        }
-
-        const precio = userFilter.priceSpecial ? listProducts[0].specialPrice : listProducts[0].price;
-
-        response.status(200).json(precio);
-
+      const { user_id, nombre_producto } = request.params;
+      if (!(user_id || nombre_producto)) {
+        return response.status(400).json({ message: "faltan datos" });
+      }
+  
+      const userFilter = await User.findById(user_id);
+  
+      if (!userFilter) {
+        return response.status(404).json({ message: "Usuario no encontrado" }); // Responde 404 si no hay usuario
+      }
+  
+      const encodedNombreProducto = encodeURIComponent(nombre_producto);
+      const decodedNombreProducto = decodeURIComponent(nombre_producto.replace(/\+/g, ' '));
+  
+      const listProducts = await Product.find({ name: decodedNombreProducto });
+  
+      // Si la lista de productos está vacía, significa que no se encontró el producto
+      if (listProducts.length === 0) {
+        return response.status(404).json({ message: "Producto no encontrado" }); // Responde 404 si no hay productos
+      }
+  
+      const precio = userFilter.priceSpecial ? listProducts[0].specialPrice : listProducts[0].price;
+  
+      response.status(200).json(precio);
+  
     } catch (error) {
-        console.error(error);
-        response.status(500).send(error);
+      console.error(error);
+      response.status(500).send(error);
     }
-};
+  };
+  
 
 module.exports = {
     getProducts,
