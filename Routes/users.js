a1d9c9b271fb7f75962b4d60e837041197b2b1a8
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2024-07-27 16:51:06 -0500

    actualizacion

--- Routes/users.js
+++ Routes/users.js
@@ -1,16 +1,27 @@
 const Router = require('express').Router();
-const {login,newStudent, getUsers,completeInfo,getUserData,getUserById,upPhoto}= require('../Controllers/Users')
+const { 
+  login, 
+  newStudent, 
+  getUsers, 
+  completeInfo, 
+  getUserData, 
+  getUserById, 
+  upPhoto,
+  updateBankDetails,
+  getBankDetails 
+} = require('../Controllers/Users');
 
+// Rutas existentes
+Router.post('/login', login);
+Router.get('/users', getUsers);
+Router.post('/signup', newStudent);
+Router.post('/userinformation', completeInfo);
+Router.get('/userdata', getUserData);
+Router.get('/user/:id', getUserById);
+Router.post('/user/update/photo', upPhoto);
 
+// Ruta para actualizar detalles bancarios
+Router.put('/user/:id/bank-details', updateBankDetails);
+Router.get('/user/:id/bank-details', getBankDetails);
 
-
-Router.post('/login',login)
-Router.get('/users',getUsers)
-Router.post('/signup',newStudent)
-Router.post('/userinformation',completeInfo)
-Router.get('/userdata',getUserData)
-Router.get('/user/:id',getUserById)
-Router.post('/user/update/photo',upPhoto)
-
-
-module.exports=Router;
+module.exports = Router;
