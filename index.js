c622cdaaf18ee622259c35eab938904cedf5ecf3
Waltza1 <111610017+Waltza1@users.noreply.github.com>
2024-08-16 13:42:52 -0500

    rutas imagenes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const path = require('path');  
 
 const connectDB = require('./dbConnection.js');
 const emailRouter = require('./Routes/email');
@@ -18,7 +19,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+//app.use('/uploads', express.static('uploads'));
 
 app.use('/api', usersRouter);
 app.use('/api', calendarRouter);
